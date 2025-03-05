06e0bbe5793ce76052df00bb0ca42fd607ecdd6f
Blurr28 <jeeveshsharma28@gmail.com>
2025-03-05 15:27:55 +0530

    Add Config class for managing configuration settings with YAML support

---
+++ config.py
@@ -0,0 +1,54 @@
+import yaml
+import json
+
+class Config:
+
+    def __init__(self, config=None):
+        
+        if isinstance(config, ):
+            for key, value in config.items():
+                setattr(self, key, value)
+        elif isinstance(config, str):
+            self._load(config)
+        elif config is None:
+            pass
+        else:
+            raise ValueError("Invalid config type")
+        
+
+    def _load(self, path):
+        with open(path, 'r') as f:
+            config = yaml.safe_load(f)
+            if isinstance(config, dict):
+                for key, value in config.items():
+                    setattr(self, key, value)
+            else:
+                raise ValueError("YAML file must contain a dictionary")
+    
+    def save(self, path):
+        with open(path, 'w') as f:
+            yaml.dump(self.__dict__, f)
+    
+    def __repr__(self):
+        return json.dumps(self.__dict__, indent=4)
+    
+    def add(self, key, value):
+        if hasattr(self, key):
+            raise ValueError(f"Key {key} already exists")
+        setattr(self, key, value)
+    
+    def fetch(self, key):
+        if hasattr(self, key):
+            return getattr(self, key)
+        else:
+            raise ValueError(f"Key {key} does not exist")
+    
+    def remove(self, key):
+        if hasattr(self, key):
+            delattr(self, key)
+        else:
+            raise ValueError(f"Key {key} does not exist")
+    
+    @property
+    def keys(self):
+        return list(self.__dict__.keys())

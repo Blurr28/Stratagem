d7f27c3e3c11d5ce2ecc8ff515f1d05bbd024f55
Blurr28 <jeeveshsharma28@gmail.com>
2025-03-09 16:56:06 +0530

    Add RulesManager class for dynamic rule loading and application

---
+++ core/rules_manager.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from rule import MovingAverageCrossoverRule, VolatilityRule
+
+class RulesManager:
+    RULE_CLASSES = {
+        "MovingAverageCrossover": MovingAverageCrossoverRule,
+        "Volatility": VolatilityRule
+    }
+
+    def __init__(self, config: dict, default_rules: list = None):
+        self.config = config
+
+        if config.get('rules'):
+            self.rules = self.load_rules_from_config(config)
+        else:
+            self.rules = default_rules if default_rules is not None else []
+        
+    def load_rules_from_config(self, config: dict) -> list :
+        rule_instances = []
+
+        for rule_config in config.get('rules', []):
+            rule_name = rule_config.get('name')
+            params = rule_config.get('params', {})
+            
+            if rule_name in self.RULE_CLASSES:
+                rule_instances.append(self.RULE_CLASSES[rule_name].from_config(params))
+            else:
+                raise ValueError(f"Unknown rule {rule_name}")
+        
+        return rule_instances
+    
+    def apply_rules(self, data: pd.DataFrame) -> pd.DataFrame:
+        results = {}
+
+        for rule in self.rules:
+            results[rule.__class__.__name__] = rule.apply(data.copy())
+        
+        return results

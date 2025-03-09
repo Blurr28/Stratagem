8b22bb0d50ca22d3283ac3d24e2a8ae9673d48f8
Blurr28 <jeeveshsharma28@gmail.com>
2025-03-09 17:14:39 +0530

    Add abstract get_parameters method to Rule class

--- core/rules/base.py
+++ core/rules/base.py
@@ -6,7 +6,12 @@ class Rule(ABC):
     def apply(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
+    @abstractmethod
+    def get_parameters(self) -> dict:
+        pass
+
     @classmethod
     @abstractmethod
     def from_config(cls, config: dict):
         pass
+
